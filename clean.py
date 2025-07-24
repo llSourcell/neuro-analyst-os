edb9e48fe27e33aadcc9f4721c45425046eb1592
Siraj Raval <llSourcell@gmail.com>
2025-07-24 14:52:24 -0400

    Feature: Add initial skeleton for cleaning pipeline

---
+++ clean.py
@@ -0,0 +1,9 @@
+def clean_eeg_data(raw_data):
+    """
+    This function will one day be the core of the cleaning pipeline.
+    For now, it's just a placeholder.
+    """
+    print("Cleaning pipeline initiated...")
+    # Future steps: filtering, artifact removal, etc.
+    cleaned_data = raw_data
+    return cleaned_data
